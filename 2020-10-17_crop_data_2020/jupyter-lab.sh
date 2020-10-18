45a2b2850fc881495abdf918cc84ecef0999228f
Trimbljk <Jtrimble@agbiome.com>
2020-10-18 00:24:26 -0400

    created api key for USDA database access

---
+++ 2020-10-17_crop_data_2020/jupyter-lab.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+while :
+do
+        PORT="`shuf -i 8888-9999 -n 1`"
+        break
+done
+
+docker run \
+    -e "NB_UID=$UID" --user root \
+    -e "GRANT_SUDO=yes" \
+    -e DATA_DIR \
+    -e USDAKEY \
+    -e STAGING_BUCKET \
+    -e TESTING \
+    --name 2020-10-17_crop_data_2020_$(hostname)_$(id -u) \
+    -w $PWD \
+    -v $PWD:$PWD \
+    -p $PORT:$PORT  -d --rm \
+    -v $HOME/.aws:/home/jovyan/.aws \
+    jkt/2020-10-17_crop_data_2020 start.sh jupyter lab --allow-root --port $PORT
